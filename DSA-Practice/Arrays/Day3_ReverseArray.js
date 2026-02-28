a8c44abc628c43ad24e047a0b049af9694dd06bd
MaheshMedia <maheshnaik758@gmail.com>
2026-02-28 21:39:59 +0530

    Day4

--- DSA-Practice/Arrays/Day3_ReverseArray.js
+++ DSA-Practice/Arrays/Day3_ReverseArray.js
@@ -1,1 +1,20 @@
 //Problem 3: Reverse an Array (In-Place)
+
+function reverseArray(arr) {
+    let left = 0;
+    let right = arr.length - 1;
+
+    while (left < right) {
+        let temp = arr[left];
+        arr[left] = arr[right];
+        arr[right] = temp;
+
+        left++;
+        right--;
+    }
+
+    return arr;
+}
+
+let nums = [1, 8, 6, 4, 5, 10];
+console.log(reverseArray(nums));
---
+++ DSA-Practice/Arrays/Day4_CheckifArrayisSorted.js
@@ -0,0 +1,1 @@
+//Problem 4: Check if Array is Sorted: Given an array, check whether it is sorted in ascending order
